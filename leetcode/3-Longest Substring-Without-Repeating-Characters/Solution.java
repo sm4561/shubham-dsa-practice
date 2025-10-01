036082b20818f3828672441b02f15ce99c9990e4
Shubham Mishra <shubham.23scse1011744@galgotiasuniversity.ac.in>
2025-10-01 14:13:30 +0530

    Update Solution.java

--- leetcode/3-Longest Substring-Without-Repeating-Characters/Solution.java
+++ leetcode/3-Longest Substring-Without-Repeating-Characters/Solution.java
@@ -1,1 +1,21 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int left = 0, maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            // If duplicate character, move left pointer until duplicate removed
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            // Add current character to set
+            set.add(s.charAt(right));
+            // Update max length
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
 
