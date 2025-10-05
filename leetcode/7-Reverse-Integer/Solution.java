45d8a0cdc71fde911ba62b8fad120f0b3e23685b
Shubham Mishra <shubham.23scse1011744@galgotiasuniversity.ac.in>
2025-10-05 11:01:49 +0530

    Update Solution.java

--- leetcode/7-Reverse-Integer/Solution.java
+++ leetcode/7-Reverse-Integer/Solution.java
@@ -1,20 +1,17 @@
-public class Solution {
-public int reverse(int x) {
-int rev = 0;
-while (x != 0) {
-int digit = x % 10;
-x /= 10;
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
 
-```
-        // Check for overflow before multiplying
-        if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE/10 && digit > 7)) return 0;
-        if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE/10 && digit < -8)) return 0;
-        
-        rev = rev * 10 + digit;
+            // Check for overflow/underflow
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && digit > 7)) return 0;
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && digit < -8)) return 0;
+
+            rev = rev * 10 + digit;
+        }
+        return rev;
     }
-    return rev;
-}
-```
-
 }
 
