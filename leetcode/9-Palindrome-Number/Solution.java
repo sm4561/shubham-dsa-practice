7c6995cb957b3b9b89b2c60de10ce1c2a0b13a53
Shubham Mishra <shubham.23scse1011744@galgotiasuniversity.ac.in>
2025-10-07 17:10:21 +0530

    Update Solution.java

--- leetcode/9-Palindrome-Number/Solution.java
+++ leetcode/9-Palindrome-Number/Solution.java
@@ -1,1 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        // Negative numbers are not palindrome
+        if (x < 0) return false;
+
+        int original = x;
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            // Check for overflow
+            if (reversed > (Integer.MAX_VALUE - digit) / 10) {
+                return false;
+            }
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+}
 
