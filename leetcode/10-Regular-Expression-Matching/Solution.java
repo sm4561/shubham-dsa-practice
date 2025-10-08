b75ade9e11f280a917abf91f3b9306d95dc43234
Shubham Mishra <shubham.23scse1011744@galgotiasuniversity.ac.in>
2025-10-08 10:31:33 +0530

    Update Solution.java

--- leetcode/10-Regular-Expression-Matching/Solution.java
+++ leetcode/10-Regular-Expression-Matching/Solution.java
@@ -1,1 +1,33 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        // Initialize patterns like a*, a*b*, a*b*c* matching empty string
+        for (int j = 2; j <= n; j += 2) {
+            if (p.charAt(j - 1) == '*' && dp[0][j - 2]) {
+                dp[0][j] = true;
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char sc = s.charAt(i - 1);
+                char pc = p.charAt(j - 1);
+
+                if (pc == '.' || pc == sc) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pc == '*') {
+                    dp[i][j] = dp[i][j - 2]; // '*' as 0 occurrence
+                    char pPrev = p.charAt(j - 2);
+                    if (pPrev == '.' || pPrev == sc) {
+                        dp[i][j] |= dp[i - 1][j]; // '*' as 1+ occurrence
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
 
