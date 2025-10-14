a9bed8fb8589952ab4eaaf1146b7619a8eba04e1
Shubham Mishra <shubham.23scse1011744@galgotiasuniversity.ac.in>
2025-10-14 12:37:56 +0530

    Update Solution.java

--- leetcode/16-3Sum-Closest/Solution.java
+++ leetcode/16-3Sum-Closest/Solution.java
@@ -1,1 +1,23 @@
-
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closestSum = nums[0] + nums[1] + nums[2];
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1, right = nums.length - 1;
+            
+            while (left < right) {
+                int currSum = nums[i] + nums[left] + nums[right];
+                
+                if (Math.abs(target - currSum) < Math.abs(target - closestSum)) {
+                    closestSum = currSum;
+                }
+                
+                if (currSum < target) left++;
+                else if (currSum > target) right--;
+                else return currSum; // exact match found
+            }
+        }
+        return closestSum;
+    }
+}
