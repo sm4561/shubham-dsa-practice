e43a415d39578117d7ed67b5afc7b2e1b147fedb
Shubham Mishra <shubham.23scse1011744@galgotiasuniversity.ac.in>
2025-11-20 15:38:22 +0530

    Update Solution.java

--- GFG/N meetings in one room/Solution.java
+++ GFG/N meetings in one room/Solution.java
@@ -1,1 +1,26 @@
+class Solution {
+    public int maxMeetings(int start[], int end[]) {
+        int n = start.length;
+        int[][] meetings = new int[n][2];
+        
+        for (int i = 0; i < n; i++) {
+            meetings[i][0] = start[i];
+            meetings[i][1] = end[i];
+        }
+        
+        Arrays.sort(meetings, (a, b) -> a[1] - b[1]);
+        
+        int count = 1;
+        int lastEnd = meetings[0][1];
+        
+        for (int i = 1; i < n; i++) {
+            if (meetings[i][0] > lastEnd) {
+                count++;
+                lastEnd = meetings[i][1];
+            }
+        }
+        
+        return count;
+    }
+}
 
