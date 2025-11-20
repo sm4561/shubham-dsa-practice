3b28b2c6221910f6349c964a70f1cc611c8f3f35
Shubham Mishra <shubham.23scse1011744@galgotiasuniversity.ac.in>
2025-11-20 15:32:55 +0530

    Update Solution.java

--- GFG/Job Sequencing Problem/Solution.java
+++ GFG/Job Sequencing Problem/Solution.java
@@ -1,38 +1,34 @@
 class Solution {
-    public int[] JobScheduling(int[] deadline, int[] profit) {
+    public ArrayList<Integer> jobSequencing(int[] deadline, int[] profit) {
         int n = deadline.length;
         int maxD = 0;
         for (int d : deadline) if (d > maxD) maxD = d;
-        Integer[] idx = new Integer[n];
-        for (int i = 0; i < n; i++) idx[i] = i;
-        java.util.Arrays.sort(idx, (a, b) -> Integer.compare(profit[b], profit[a]));
-        int[] parent = new int[maxD + 1];
-        for (int i = 0; i <= maxD; i++) parent[i] = i;
-        java.util.function.IntUnaryOperator find = new java.util.function.IntUnaryOperator() {
-            public int applyAsInt(int x) {
-                int root = x;
-                while (root != parent[root]) root = parent[root];
-                while (x != root) {
-                    int next = parent[x];
-                    parent[x] = root;
-                    x = next;
-                }
-                return root;
-            }
-        };
-        int count = 0;
-        int total = 0;
+        
+        int[][] jobs = new int[n][2];
         for (int i = 0; i < n; i++) {
-            int j = idx[i];
-            int d = Math.min(deadline[j], maxD);
-            int avail = find.applyAsInt(d);
-            if (avail > 0) {
-                count++;
-                total += profit[j];
-                parent[avail] = find.applyAsInt(avail - 1);
+            jobs[i][0] = deadline[i];
+            jobs[i][1] = profit[i];
+        }
+        
+        Arrays.sort(jobs, (a, b) -> b[1] - a[1]);
+        
+        int[] slot = new int[maxD + 1];
+        int count = 0, total = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int t = jobs[i][0]; t > 0; t--) {
+                if (slot[t] == 0) {
+                    slot[t] = 1;
+                    count++;
+                    total += jobs[i][1];
+                    break;
+                }
             }
         }
-        return new int[]{count, total};
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        ans.add(count);
+        ans.add(total);
+        return ans;
     }
 }
-
